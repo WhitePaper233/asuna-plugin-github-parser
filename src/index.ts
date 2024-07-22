d9527f7c86cc1172ae096df6217e3e3a7afb46cb
WhitePaper233 <1423634265@qq.com>
2024-07-22 16:27:37 +0800

    feat: 实现 Github 仓库链接解析

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,37 @@
-import { Context, Schema } from 'koishi'
+import { Context, h, Schema } from 'koishi'
 
 export const name = 'github-parser'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
+const GITHUB_REPO_REGEX = /https:\/\/github\.com\/([^/]+)\/([^/]+)\/?/
+
+class GithubParser {
+    constructor(ctx: Context) {
+        ctx.intersect(session => GITHUB_REPO_REGEX.test(session.content))
+            .middleware(async (session, next) => {
+                // match repo owner and name
+                const match = session.content.match(GITHUB_REPO_REGEX)
+                if (!match) return next()
+                const [, owner, repo] = match
+
+                try {
+                    // check repo existance
+                    await ctx.http.get(`https://github.com/${owner}/${repo}`)
+                    // send repo info
+                    session.send(h('img', { src: `https://opengraph.githubassets.com/0/${owner}/${repo}` }))
+                }
+                catch (e) {
+                    return next()
+                }
+
+                return next()
+            })
+    }
+}
+
 export function apply(ctx: Context) {
-  // write your plugin here
+    ctx.plugin(GithubParser)
 }
